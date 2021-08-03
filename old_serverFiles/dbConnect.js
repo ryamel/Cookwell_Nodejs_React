bdd2adafae1f0523da6ed21c72efcad9ab56108d
Ryan Melnick <45801157+ryamel@users.noreply.github.com>
2021-08-02 17:12:29 -0700

    clean up ignored files

--- old_serverFiles/dbConnect.js
+++
@@ -1,3 +0,0 @@
-mongoose.connect('mongodb://localhost/cookwell', { useNewUrlParser: true } ) // this will be different for production. It will come from config file
-	.then(() => console.log('Connected to MongoDB...'))
-	.catch(err => console.error('Could not connect to MongoDB...', err));
