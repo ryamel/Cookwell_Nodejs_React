51106b084b62ace754a547b2e5b59d4f72d78a31
Ryan Melnick <45801157+ryamel@users.noreply.github.com>
2021-08-04 16:18:11 -0700

    add

--- server.js
+++ server.js
@@ -41,14 +41,14 @@ const db = mongoose.connect(process.env.DB_connection, {
 // load production middleware
  // require('./middleware/prod')(app); 
 
-// Have Nodejs serve the static files from the React app (needed for production build)
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// A result of using react Router. The server trys to serve up static html pages for each page. But all pages are handles in index.html....
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder...Have Nodejs serve the static files from the React app (needed for production build)
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	// A result of using react Router. The server trys to serve up static html pages for each page. But all pages are handles in index.html....
+	app.get('*', (req, res) => {
+	    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	});
+}
 
 
 
