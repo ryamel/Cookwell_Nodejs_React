d4f1e7be84e9df434cf1a03f29ff54f3bc6300cf
Ryan Melnick <45801157+ryamel@users.noreply.github.com>
2021-08-05 23:08:32 -0700

    add

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const db = mongoose.connect(process.env.DB_connection, {
 
 //if (process.env.NODE_ENV === 'production') {
 	// Set static folder...Have Nodejs serve the static files from the React app (needed for production build)
-	 app.use(express.static(path.join(__dirname,'client','build')));
+	app.use(express.static(path.join(__dirname,'client','build')));
 	// A result of using react Router. The server trys to serve up static html pages for each page. But all pages are handles in index.html....
 	app.get('/*', (req, res) => {
 	    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
