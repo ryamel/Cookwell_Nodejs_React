3b538429e6db9e74528963db82d5079a83d8ec58
Ryan Melnick <45801157+ryamel@users.noreply.github.com>
2021-09-24 17:32:02 -0700

    update dep

---
+++ client/src/components/incomplete_socialBanner.js
@@ -0,0 +1,14 @@
+					{/*	{	this.displayDietBanner(this.state.result.diet)	}*/}
+
+	{/*					<div id="share-this-container">
+
+							<div id="title">Share this recipe</div>
+
+							<div id="ad-flexbox-container">
+								<div><a href="#" className="fa fa-facebook"></a></div>
+								<div><a href="#" className="fa fa-twitter"></a></div>
+								<div><a href="#" className="fa fa-pinterest"></a></div>
+								<div><a href="#" className="fa fa-envelope"></a></div>
+							</div>
+
+						</div>*/}
